ae76290c5c8d0bc981e08c72e999748ae68c9b5b
ktsorensenWX <82471248+ktsorensenWX@users.noreply.github.com>
2021-06-01 11:28:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import argparse, sys
+import pandas as pd
+import matplotlib.pyplot as plt
+from get_data import SeaTemps
 
+
+def main():
+    nh_sea_data = SeaTemps()
+    data_parser = argparse.ArgumentParser(description='Compiling Hurricane Initiation with Sea Temperatures')
+    data_parser.add_argument('command', metavar='<command>', choices=['print'], type=str,
+                             help='command to execute')
+    data_parser.add_argument('-o', '--ofile', metavar='<outfile>', dest='ofile', action='store')
+    data_parser.add_argument('-p', '--plot', action='store_true', dest='plot')
+    args = data_parser.parse_args()
