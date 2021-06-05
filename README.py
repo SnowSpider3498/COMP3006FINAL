e4ec809c1cc36ce827edc49c018eb3729d304b07
ktsorensenWX <82471248+ktsorensenWX@users.noreply.github.com>
2021-06-05 10:35:27 -0600

    Create README.py

---
+++ README.py
@@ -0,0 +1,49 @@
+# Final Project: Hurricane Frequency and Intensity and the correlation with Ocean Tempuratures
+
+## Group Members
+Kristoffer Sorensen & Courtnay McClure
+
+## Research Question
+<br /> <br />
+
+## Datasets utilized
+Salary data scraped from 
+
+Points data scraped from #[ESPN](https://www.espn.com/nba/stats/team/_/season/2017/seasontype/2)
+<br /> <br />
+
+## Analysis Conclusion
+
+<br /> <br />
+
+## Usage
+```bash
+python nba_covid_analysis.py [command] [optional arguments]
+```
+### Dependencies
+package dependencies listed in requirements.txt
+
+### [command]
+
+# **data_eda**: explores salary and points data
+
+# **covid_analysis**: explores salary per points
+
+# ### [optional arguments]
+
+# -**load_from_file**: loads jsons for salary and points data from folder. input will be requested from terminal
+
+# -**p**: plots figures associated with command
+
+# -**csv** *folder name*: saves csvs associated with command to data/folder name
+
+# -**json** *folder name*: saves jsons accociated with command to data/folder name
+
+# -**p**: plots figures associated with command
+
+# -**start_year** *year*: utilizes this year as beginning year to scrape webdata
+
+# -**end_year** *year*: utilized this year as end year to scrape webdata
+
+# -**flask_app**: runs local flask application to display plots 
+
