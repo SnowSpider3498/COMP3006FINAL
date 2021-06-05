c011cef2e656d2105c3cd8134f853ddf4ddd5c97
ktsorensenWX <82471248+ktsorensenWX@users.noreply.github.com>
2021-06-04 19:15:43 -0600

    Update refactor_sea_data.py

--- refactor_sea_data.py
+++ refactor_sea_data.py
@@ -1,3 +1,4 @@
+# This class is here to only change the types of the desired variables and give them a format for printing
 class DisplaySeaTemps:
     def __init__(self, year, avg_anomaly, lower_confidence, upper_confidence):
         self.year = int(year)
@@ -11,13 +12,6 @@ class DisplaySeaTemps:
     def __str__(self):
         return self.__repr__()
 
-    # def __gt__(self, other):
-    #     if type(self) == type(other):
-    #         if self.avg_anomaly > other.avg_anomaly:
-    #             return (self.year, self.avg_anomaly, self.lower_confidence, self.upper_confidence) > \
-    #                    (other.year, other.avg_anomaly, other.lower_confidence, other.upper_confidence)
-    #     else:
-    #         return NotImplemented
-
     def __hash__(self):
         return hash((self.year, self.avg_anomaly, self.lower_confidence, self.upper_confidence))
+
