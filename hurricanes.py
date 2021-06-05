861621684b31bd2e0c111f828967497c4cee5b39
SnowSpider3498 <courtmcclur@gmail.com>
2021-06-05 11:42:14 -0500

    update

--- hurricanes.py
+++ hurricanes.py
@@ -38,8 +38,8 @@ class StormData:
 
         #define attributes
         headers = []
-        self.data = []
-        dictData = {} #should i have it as a dictionary? or a list? then i can utilize the class data similar to autompg.
+        # self.data = []
+        self.dictData = {} #should i have it as a dictionary? or a list? then i can utilize the class data similar to autompg.
         # define algorithm for correct data pull
         singles = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21, 22, 23, 24, 25, 26, 28, 30, 31, 32, 33, 34, 37, 38, 39, 41, 43, 44, 45, 46, 49, 51, 53, 54, 56, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 71, 72, 74, 76, 77, 78, 79, 84, 87, 88, 89, 90, 95, 96, 97, 101, 105, 106, 109, 111, 112, 114, 116, 117, 121, 122, 124, 126, 128, 131, 132, 135, 136, 140, 141, 142, 143, 146, 158, 163]
         doubSing = [18, 29, 40, 47, 48, 50, 52, 55, 57, 58, 73, 75, 80, 81, 83, 85, 86, 91, 92, 93, 94, 98, 100, 102, 103, 104, 107, 108, 110, 113, 115, 119, 120, 123, 125, 127, 129, 130, 133, 134, 137, 138, 139, 145, 148, 149, 150, 151, 152, 153, 155, 156, 157, 160, 162, 164, 165]
@@ -66,8 +66,8 @@ class StormData:
                         hurricane = dat[5]
                         majhurricane = dat[6:]
 
-                        # dictData[year]=(nameStorm, hurricane, majhurricane)
-                        self.data.append(Storm(year, nameStorm, hurricane, majhurricane))
+                        self.dictData[year]=(nameStorm, hurricane, majhurricane)
+                        # self.data.append(Storm(year, nameStorm, hurricane, majhurricane))
                     # double - single algorithm
                     elif idx in doubSing:
                         year = dat[:4]
@@ -75,8 +75,8 @@ class StormData:
                         hurricane = dat[6]
                         majhurricane = dat[7:]
 
-                        # dictData[year] = (nameStorm, hurricane, majhurricane)
-                        self.data.append(Storm(year, nameStorm, hurricane, majhurricane))
+                        self.dictData[year] = (nameStorm, hurricane, majhurricane)
+                        # self.data.append(Storm(year, nameStorm, hurricane, majhurricane))
                     # double - double algorithm
                     elif idx in doubDoub:
                         year = dat[:4]
@@ -84,18 +84,47 @@ class StormData:
                         hurricane = dat[6:8]
                         majhurricane = dat[8:]
 
-                        # dictData[year] = (nameStorm, hurricane, majhurricane)
-                        self.data.append(Storm(year, nameStorm, hurricane, majhurricane))
+                        self.dictData[year] = (nameStorm, hurricane, majhurricane)
+                        # self.data.append(Storm(year, nameStorm, hurricane, majhurricane))
 
-        print(self.data)
+        
 
 
     def graphStorm(self):
 
-        pass
-                
-        # print(headers)
+        self.yrs = self.dictData.keys()
+        self.named = []
+        self.hurr = []
+        self.maj = []
+
+        for i in self.dictData.values():
+            self.named.append(i[0])
+            self.hurr.append(i[1])
+            self.maj.append(i[2])
             
+        plt.style.use('dark_background')
+        plt.plot(self.yrs, self.named, color='deeppink')
+        plt.title('Named Storms per Year', color='white')
+        plt.xlabel('Year', color='white')
+        plt.ylabel('Named Storms', color='white')
+        plt.xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 158, 166], rotation = 75, fontsize = 7)
+        plt.tight_layout()
+        plt.show()
+        plt.savefig('stormsperyear')
+
+    def graphHurricanes(self):
+
+        plt.style.use('dark_background')
+        plt.bar(self.yrs, self.hurr, color='green')
+        plt.bar(self.yrs, self,maj, bottom=self.hurr, color='red')
+        plt.title('Hurricanes per Year', color='white')
+        plt.xlabel('Year', color='white')
+        plt.ylabel('Named Storms', color='white')
+        plt.xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 158, 166], rotation=75, fontsize=7)
+        plt.yticks(fontsize=7)
+        plt.tight_layout()
+        plt.show()
+        plt.savefig('hurricanes_majperyear')
 
 
 # pretend main
@@ -105,6 +134,10 @@ def main():
     collectData = StormData()
 
     collectData._get_data()
+
+    collectData.graphStorm()
+
+    # collectData.graphHurricanes()
 
 
 if __name__ == '__main__':
