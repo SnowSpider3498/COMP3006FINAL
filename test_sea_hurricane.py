c6ea3189be99891ffcde3b98bc57b77f3fae4d20
ktsorensenWX <82471248+ktsorensenWX@users.noreply.github.com>
2021-06-05 21:49:54 -0600

    Update test_sea_hurricane.py

--- test_sea_hurricane.py
+++ test_sea_hurricane.py
@@ -1,10 +1,25 @@
 import unittest
+from refactor_data import *
+import get_data
+from data_manipulation import *
 from main import *
 
-
 class TestMain(unittest.TestCase):
-
     def test_print(self):
+        
+  
+class TestSeaTemps(unittest.TestCase):
+    def test_iterable(self):
+        iter(get_data.SeaTemps())
+
+    def test_get_data(self):
+        # make sure the website returns a status code
+        self.assertTrue(get_data.SeaTemps().response == 200)
+
+
+class TestDataManipulation(unittest.TestCase):
+    def test_data_manipulation(self):
+        pass
 
 
 if __name__ == '__main__':
