421d7dc0b087ec127d518f947fe6ae1b4d55441a
ktsorensenWX <82471248+ktsorensenWX@users.noreply.github.com>
2021-06-05 22:26:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ from plot import *
 
 
 def main():
+    # Instantiate both objects 
     nh_sea_data = SeaTemps()
     storm_data = StormData()
 
+    # We removed -p because a plot is automatically generated when saved to csv, explained more in README
     data_parser = argparse.ArgumentParser(description='Compiling Hurricane Initiation with Sea Temperatures')
     data_parser.add_argument('command', metavar='<command>',
                              choices=['print', 'by_decade', 'merge_storms', 'merge_majors'], type=str,
                              help='command to execute')
     data_parser.add_argument('-o', '--ofile', metavar='<outfile>', dest='ofile', action='store')
-    data_parser.add_argument('-p', '--plot', action='store_true', dest='plot')
     data_parser.add_argument('-s', '--sort', metavar='<sort>', choices=['confidence', 'anomaly', 'merge', 'tropical'],
                              dest='sort')
     args = data_parser.parse_args()
 
     if args.command == 'print':
-        
         head_row = [['Year', 'Annual Anomaly', 'Lower Confidence Interval', 'Upper Confidence Interval']]
         anomaly_row, confidence_row = [['Year', 'Annual Anomaly']], [['Year', 'Lower', 'Upper']]
         tropical_row = [['Year', 'Named Storms', 'Hurricanes', 'Major Hurricanes']]
-
+        
+        # Checks for sorting arguments 
         if args.sort is None:
             for x in nh_sea_data:
                 row = [x.year, x.avg_anomaly, x.lower_confidence, x.upper_confidence]
@@ -42,6 +43,7 @@ def main():
                 row4 = [x.year, x.storms, x.hurricanes, x.majors]
                 tropical_row.append(row4)
 
+        # Checks for an outfile by user choice
         if args.ofile is None:
             if args.sort is None:
                 to_stdout = csv.writer(sys.stdout)
@@ -57,6 +59,7 @@ def main():
                 to_stdout.writerows(tropical_row)
 
         else:
+            # Writes to csv of user choice
             with open(args.ofile, 'w', newline='') as optimised_sst:
                 to_file = csv.writer(optimised_sst, quoting=csv.QUOTE_ALL)
                 if args.sort is None:
@@ -76,6 +79,7 @@ def main():
                         graphStorm(storm_data.hurricane_values)
                         graph_severe_hurricanes(storm_data.hurricane_values)
 
+    # This command focuses only on decade information for sea temperatures
     if args.command == 'by_decade':
         if args.sort is None:
             if args.ofile is None:
@@ -118,21 +122,18 @@ def main():
 
             if args.plot is not None:
                 plot_decade_confidence(confidence)
-
+                
+        # Merges both anomalies and confidence in one graph, mainly used to look at technological accuracy over time
         elif args.sort == 'merge':
             if args.plot is not None:
                 merged = merge(nh_sea_data.sea_values)
                 merge_decade(merged)
 
-    # if args.command == 'storm':
-    #     graphStorm(StormData.hurricane_values)
-
-    # if args.command == 'severe':
-    #     graph_severe_hurricanes(StormData.hurricane_values)
-
+    # merges both anomalies and tropical storm values in one graph <command>
     if args.command == 'merge_storms':
         combine_anomaly_storms(StormData.hurricane_values, SeaTemps.sea_values)
 
+    # merges both anomalies and major hurricanes in one graph <command>
     if args.command == 'merge_majors':
         combine_anomaly_majors(StormData.hurricane_values, SeaTemps.sea_values)
 
